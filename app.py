fcd358c28a4015923aa0a873b4d89b5626a0c55c
Tom Sharp <tomsharp924@gmail.com>
2025-11-02 18:21:57 -0800

    Updated admin to better represent data

--- app.py
+++ app.py
@@ -4,30 +4,25 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv                                                    #type: ignore
+import datetime
 import json
 import os
 
-load_dotenv()
-db_link = os.getenv("DATABASE_URL")
 app = Flask(__name__)
 # Allow cross referencing from port 5000 (main)
 CORS(app, origins=["http://127.0.0.1:5000"])
 
-client = MongoClient(db_link, server_api=ServerApi('1'))
-db = client.email_microservice
-
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
-# Fix user IP address so we can track who requests came from. 
-app.wsgi_app = ProxyFix(
-    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
-)
-
 adminCode = "123"
+
+@app.template_filter('friendly_datetime')
+def friendly_datetime(value, format="%B %d, %Y at %I:%M %p"):
+    from datetime import datetime
+    if not value:
+        return "NULL"
+    if isinstance(value, str):
+        # Convert ISO string to datetime
+        value = datetime.fromisoformat(value.replace("Z", "+00:00"))
+    return value.strftime(format)
 
 @app.route("/")
 def index():
@@ -43,11 +38,25 @@ def renderDebugMode():
 
 @app.route("/admin/<access_code>")
 def adminPannel(access_code):
-    if(access_code == adminCode):
+    if access_code != adminCode:
+        return redirect(url_for("index"))
+    
+    view = request.args.get("view")
+    if view is None:
+        # Redirect to same route with view="emails"
+        return redirect(url_for("adminPannel", access_code=access_code, view="emails"))
+
+    if view == "emails":
         with open("./static/email-test.json", "r", encoding='utf-8') as f:
             data = json.load(f)
-        return render_template("admin.html", email_data=data)
-    return redirect(url_for("index"))
+        return render_template("admin-emailsView.html", email_data=data, access_code=access_code)
+    elif view == "timed_emails":
+        with open("./static/timed-email-test.json", "r", encoding='utf-8') as f:
+            data = json.load(f)
+        return render_template("admin-timedEmailsView.html", email_data=data, access_code=access_code)
+
+
+
 
 @app.route('/ping')
 def ping():
